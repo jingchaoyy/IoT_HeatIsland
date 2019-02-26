f40d8e2865a52d17a865ab518f4720730970ea4e
Jingchao Yang <jcyangt@gmail.com>
2019-02-26 14:56:08 -0500

    initialized, having trouble with GCP authentication

---
+++ test/Tracking_City_Temperature_by_Hour.py
@@ -0,0 +1,60 @@
+"""
+Created on  2/25/2019
+@author: Jingchao Yang
+"""
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime
+import numpy as np
+import datalab.bigquery as bq
+import pandas as pd
+
+import os
+
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "../IoT-UrbanInfo-f202ef7b61db.json"
+
+# SQL = """
+#       SELECT LocalDate, LocalHour, AVG(Temperature_C) as AvgTemperature
+#         FROM `geotab-public-intelligence.Weather.Temperature`
+#        WHERE State LIKE 'North Carolina'
+#          AND City LIKE 'Charlotte'
+#        GROUP BY LocalDate, LocalHour
+#        ORDER BY LocalDate, LocalHour
+# """
+#
+# df = bq.Query(SQL).to_dataframe(dialect='standard')
+df = pd.read_csv('../results-20190226-143739.csv')
+print(df)
+
+
+def temperature_plot(date, hour, temperature):
+    dates = [' '.join(x) for x in zip(date, [str(v).zfill(2) for v in hour])]
+    dates = [datetime.datetime.strptime(x, "%Y-%m-%d %H") for x in dates]
+    locator = mdates.HourLocator()
+    formatter = mdates.DateFormatter('%Y-%m-%d - %H:00')
+
+    fig, ax = plt.subplots(ncols=1, figsize=(30, 5))
+    ax.scatter(dates, temperature, s=300, c=temperature, cmap='plasma', zorder=2)
+    ax.plot(dates, temperature, '--k', zorder=1)
+
+    ax.set_ylim([np.floor(min(temperature)) - 1, np.ceil(max(temperature)) + 1])
+    ax.set_xlim([min(dates), max(dates)])
+    ax2 = ax.twinx()
+    ax2.set_ylim([ax.get_ylim()[0] / 0.5556 + 32, ax.get_ylim()[1] / 0.5556 + 32])
+
+    ylabel_left = 'Temperature ($^\circ$C)'
+    ylabel_right = 'Temperature ($^\circ$F)'
+    ax.set_xlabel('Date and Time', fontsize=14, labelpad=20)
+    axlab = ax.set_ylabel(ylabel_left, fontsize=14, labelpad=10)
+    ax2lab = ax2.set_ylabel(ylabel_right, fontsize=14, labelpad=10)
+    ax2.grid(None)
+
+    fig.autofmt_xdate()
+    ax.xaxis.set_major_locator(locator)
+    ax.xaxis.set_major_formatter(formatter)
+
+    return plt
+
+
+a = temperature_plot(df['LocalDate'], df['LocalHour'], df['AvgTemperature'])
+a.show()
