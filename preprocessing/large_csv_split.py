057b32373c72f02c51fb49db8563987a3426c6e6
Jingchao Yang <jcyangt@gmail.com>
2019-04-25 10:00:00 -0400

    adding func. for split by time

--- preprocessing/large_csv_split.py
+++ preprocessing/large_csv_split.py
@@ -3,9 +3,10 @@ Created on  4/15/2019
 @author: Jingchao Yang
 """
 import sys
+import pandas as pd
 
 
-def split_file(path, fname, n_split):
+def split_file_by_line(path, fname, n_split):
     """
 
     :param path:
@@ -19,11 +20,86 @@ def split_file(path, fname, n_split):
     file = 1
     for j in range(len(csvfilename)):
         if j % n_split == 0:
-            open(str(path) + "\\split\\" + str(file) + '.csv', 'w+').writelines(csvfilename[j:j + n_split])
+            open(str(path) + "\\split_line\\" + str(file) + '.csv', 'w+').writelines(csvfilename[j:j + n_split])
             file += 1
 
 
-path = 'D:\\IoT_HeatIsland\\AoT_data\\arrayOfThings_Chicago\\zzLatest\\AoT_Chicago.complete.latest\\AoT_Chicago.complete.2019-03-30\\data.csv\\'
-# path = 'D:\\IoT_HeatIsland\AoT_data\\arrayOfThings_Chicago\\chicago-complete.daily.2019-04-14\\data.csv\\'
+def split_file_by_date(path, fname):
+    """
+
+    :param path:
+    :param fname:
+    :return:
+    """
+    datePool, dateGroup, dataFlag = [], [], '1900-0-0'
+    with open(path + fname, 'r') as file:
+        for cnt, line in enumerate(file):
+            tstamp = line
+            date = tstamp.split()[0]
+            if date in datePool:
+                dateGroup.append(line)
+            else:
+                open(str(path) + "\\split_date\\" + str(dataFlag) + '.csv', 'w+').writelines(dateGroup)
+                dataFlag = date
+                dateGroup = []
+                datePool.append(date)
+                dateGroup.append(line)
+
+
+def split_file_by_hour(path, fname):
+    """
+
+    :param path:
+    :param fname:
+    :return:
+    """
+    datePool, dateGroup, dataFlag = [], [], '1900-0-0'
+    with open(path + fname, 'r') as file:
+        for cnt, line in enumerate(file):
+            if cnt > 0:
+                tstamp = line
+                date_hour = tstamp.split(':')[0]
+                if date_hour in datePool:
+                    dateGroup.append(line)
+                else:
+                    open(str(path) + "\\split_hour\\" + str(dataFlag) + '.csv', 'w+').writelines(dateGroup)
+                    dataFlag = date_hour
+                    dateGroup = []
+                    datePool.append(date_hour)
+                    dateGroup.append(line)
+
+
+def split_file_by_minute(path, fname):
+    """
+
+    :param path:
+    :param fname:
+    :return:
+    """
+    datePool, dateGroup, dataFlag = [], [], '1900-0-0'
+    with open(path + fname, 'r') as file:
+        for cnt, line in enumerate(file):
+            if cnt > 0:
+                tstamp = line
+                datetime_tmp = tstamp.split(':')[0]
+                minute = tstamp.split(':')[1]
+                datetime = datetime_tmp + ':' + minute
+                if datetime in datePool:
+                    dateGroup.append(line)
+                else:
+                    id = str(dataFlag)
+                    id = id.replace('/', '-')
+                    id = id.replace(':', '-')
+                    open(str(path) + "\\splitOriginal\\split_time\\" + id + '.csv', 'w+').writelines(
+                        dateGroup)
+                    dataFlag = datetime
+                    dateGroup = []
+                    datePool.append(datetime)
+                    dateGroup.append(line)
+
+
+path = 'D:\\IoT_HeatIsland\\AoT_data\\arrayOfThings_Chicago\\chicago-complete.monthly.2018-08-01-to-2018-08-31\\data.csv\\'
 fileName = 'data.csv'
-split_file(path, fileName, 1000000)
+# fileName = 'data.csv'
+# split_file_by_line(path, fileName, 1000000)
+split_file_by_minute(path, fileName)
