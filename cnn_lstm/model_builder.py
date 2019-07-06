a19ea08950c4cf62f2d047c8c321fdb333880931
jingchao yang <jcyangt@gmail.com>
2019-07-06 17:16:12 -0400

    merged cnn_lstm for time series only (no spatial)

--- cnn_lstm/model_builder.py
+++ cnn_lstm/model_builder.py
@@ -1,6 +1,12 @@
 """
 Created on  2019-07-06
 @author: Jingchao Yang
+
+https://machinelearningmastery.com/how-to-get-started-with-deep-learning-for-time-series-forecasting-7-day-mini-course/
+
+Using CNN and LSTM for simple time series prediction only, as the model can support very long input sequences that can
+be read as blocks or subsequences by the CNN model, then pieced together by the LSTM model.
+The CNN model interprets each sub-sequence and the LSTM pieces together the interpretations from the subsequences
 """
 # univariate cnn example
 import cnn_lstm.data_prep as input
@@ -10,29 +16,27 @@ from keras.layers import Dense
 from keras.layers import Flatten
 from keras.layers.convolutional import Conv1D
 from keras.layers.convolutional import MaxPooling1D
+from keras.layers import TimeDistributed
 from keras.layers import LSTM
 
 # define dataset
-# X = array([[10, 20, 30], [20, 30, 40], [30, 40, 50], [40, 50, 60]])
-# y = array([40, 50, 60, 70])
 X = array(input.X)
 y = array(input.y)
 
-# reshape from [samples, timesteps] into [samples, timesteps, features]
-X = X.reshape((X.shape[0], X.shape[1], 1))
+# reshape from [samples, timesteps] into [samples, subsequences, timesteps, features]
+X = X.reshape((X.shape[0], 2, 2, 1))
 # define model
 model = Sequential()
-# model.add(Conv1D(filters=64, kernel_size=2, activation='relu', input_shape=(3, 1)))
-# model.add(MaxPooling1D(pool_size=2))
-# model.add(Flatten())
-# model.add(Dense(50, activation='relu'))
-model.add(LSTM(50, activation='relu', input_shape=(4, 1)))
+model.add(TimeDistributed(Conv1D(filters=64, kernel_size=1, activation='relu'), input_shape=(None, 2, 1)))
+model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
+model.add(TimeDistributed(Flatten()))
+model.add(LSTM(50, activation='relu'))
 model.add(Dense(1))
 model.compile(optimizer='adam', loss='mse')
 # fit model
 model.fit(X, y, epochs=1000, verbose=0)
 # demonstrate prediction
 x_input = array([37, 52, 48, 55])
-x_input = x_input.reshape((1, 4, 1))
+x_input = x_input.reshape((1, 2, 2, 1))
 yhat = model.predict(x_input, verbose=0)
 print(yhat)
