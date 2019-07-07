cb1d1f0fd265509e6878249c81a16c2a4a5b3d5d
jingchao yang <jcyangt@gmail.com>
2019-07-07 18:25:31 -0400

    tested on temp data, result not good

--- cnn_lstm/model_builder.py
+++ cnn_lstm/model_builder.py
@@ -25,26 +25,32 @@ temp = pd.read_csv("tempRecord_byCol_test.csv")
 col_name = '0'
 temp_s0 = temp[col_name]
 
-input_number, output_num = 4, 1
+input_number, output_num = 24, 2
 X, y, Xy = input.to_timeseries(temp_s0, input_number, output_num)
 X = array(X)
 y = array(y)
 print(Xy)
 
 # reshape from [samples, timesteps] into [samples, subsequences, timesteps, features]
-X = X.reshape((X.shape[0], 2, 2, 1))
+samples = X.shape[0]
+subsequences = 2
+timesteps = int(X.shape[1] / 2)
+features = 1
+X = X.reshape(samples, subsequences, timesteps, features)
 # define model
 model = Sequential()
-model.add(TimeDistributed(Conv1D(filters=64, kernel_size=1, activation='relu'), input_shape=(None, 2, 1)))
+model.add(TimeDistributed(Conv1D(filters=64, kernel_size=1, activation='relu'),
+                          input_shape=(None, timesteps, features)))
 model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
 model.add(TimeDistributed(Flatten()))
 model.add(LSTM(50, activation='relu'))
-model.add(Dense(1))
+model.add(Dense(output_num))
 model.compile(optimizer='adam', loss='mse')
 # fit model
 model.fit(X, y, epochs=1000, verbose=0)
 # demonstrate prediction
-x_input = array([63.4, 62.2, 65.1, 64.7])
-x_input = x_input.reshape((1, 2, 2, 1))
+x_input = array([59.2, 58.7, 58.7, 58.91502415, 58.82705249, 58.98935525, 59.7, 56.9, 59.5, 60.3,
+                 61.5, 64.8, 65.6, 68.6, 69.5, 71.5, 68.8, 68.8, 68.0, 63.9, 64.2, 63.4, 62.2, 65.1])
+x_input = x_input.reshape(1, subsequences, timesteps, features)
 yhat = model.predict(x_input, verbose=0)
 print(yhat)
