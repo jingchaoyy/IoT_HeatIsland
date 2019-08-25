7c2ce31886d3ad29e54427d16f9bb2648e974028
pengyuli <buaalipengyu@126.com>
2019-08-24 20:49:00 -0400

    syspath

--- Temp_pred_lpy/cnn.py
+++ Temp_pred_lpy/cnn.py
@@ -14,6 +14,7 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader,Dataset,TensorDataset
 
 sys.path.append(sys.path[0]+'/../')
+print(sys.path)
 
 device = torch.device('cpu')
 
