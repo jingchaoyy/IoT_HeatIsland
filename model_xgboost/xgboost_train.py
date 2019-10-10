95f8bae8079eb833bc06df19e0e85586f21ad90d
Jingchao Yang <jcyangt@gmail.com>
2019-10-10 13:08:33 -0400

    updated predict_multiple function, debug needed

--- model_xgboost/xgboost_train.py
+++ model_xgboost/xgboost_train.py
@@ -6,9 +6,9 @@ from model_xgboost.data_tool import *
 
 
 class xgboost_tree:
-    def __init__(self, model, test_data_y):
+    def __init__(self, model, test_data_x, test_data_y):
         self.model = model
-        # self.test_data_x = test_data_x
+        self.test_data_x = test_data_x
         self.test_data_y = test_data_y
 
 
@@ -79,7 +79,6 @@ def get_trees(kernal, timestep, trainALL, testALL):
     print('start building trees')
     corner = [0, 1, 2, 3]  # 0,1,2,3 are corner of topleft, topright, botleft, botright respectively
     forest = []
-    all_test_x = []
     for cor in corner:
         print('trees for corner', cor)
         trees = []
@@ -94,39 +93,54 @@ def get_trees(kernal, timestep, trainALL, testALL):
                 cur_test_x = cur_test_x.reshape(-1, timestep * kernal * kernal)
 
                 cur_model = xgboost_model(cur_train_x, cur_train_y, cur_test_x, cur_test_y)
-                if cor == 0:
-                    all_test_x.append(cur_test_x)  # to avoid saving duplicated test_x data
 
-                tree = xgboost_tree(cur_model, cur_test_y)
+                tree = xgboost_tree(cur_model, cur_test_x, cur_test_y)
                 trees.append(tree)
         forest.append(trees)
 
-    return forest, all_test_x
+    return forest
 
 
-def predict_multiple(boosters, testdata, prediction_len):
+def predict_multiple(boosters, prediction_len, kernal, win_l, win_w):
     """
 
     :param boosters:
-    :param testdata:
     :param prediction_len:
+    :param kernal:
+    :param win_l:
+    :param win_w:
     :return:
     """
     prediction_seqs_all = []
-    for i in range(int(len(testdata[0]) / prediction_len)):
+    for i in range(int(len(boosters[0].test_data_x.shape[0]) / prediction_len)):
         prediction_seqs = []
         for k in range(prediction_len):
             predicted = []
             for j in range(len(boosters)):
-                if j == 0:
-                    curr_frame = testdata[j][i * prediction_len]
+                rolling_x = boosters[j].test_data_x[i * prediction_len]
+                rolling_y = boosters[j].test_data_y[i * prediction_len]
 
-                data_test = xgb.DMatrix(curr_frame, label=boosters[j].test_data_y)
+                data_test = xgb.DMatrix(rolling_x, label=rolling_y)
                 y_pred = boosters[j].model.predict(data_test)
                 predicted.append(y_pred)
 
-            curr_frame = curr_frame[int(curr_frame / prediction_len):]  # remove the t0 data (7*7)
-            curr_frame = curr_frame + predicted  # add the newly predicted (t+1) to update the curr_frame
+            boosters = np.array(boosters)
+            boosters.reshape(win_l, win_w)
+            predicted = np.array(predicted)
+            predicted.reshape(kernal, kernal)
+            for dx in range(boosters.shape[-1] - kernal + 1):
+                for dy in range(boosters.shape[-1] - kernal + 1):
+                    # get each 7*7 predicted from 18*18
+                    pre = predicted[dy:dy + kernal, dx:dx + kernal]
+                    # remove the t0 data (7*7)
+                    boosters[dy:dy + kernal, dx:dx + kernal].test_data_x[i * prediction_len] = \
+                        boosters[dy:dy + kernal, dx:dx + kernal].test_data_x[i * prediction_len][1:]
+                    # add the newly predicted (t+1) to update the boosters
+                    boosters[dy:dy + kernal, dx:dx + kernal].test_data_x[i * prediction_len] = \
+                        boosters[dy:dy + kernal, dx:dx + kernal].test_data_x[i * prediction_len] + [pre]
+
+            boosters.reshape(1, win_l * win_w)
+            predicted.reshape(1, kernal * kernal)
             prediction_seqs.append(predicted)  # store each predicted 18*18 frame for a prediction_len
         prediction_seqs_all.append(prediction_seqs)
     return prediction_seqs_all
@@ -160,8 +174,8 @@ if __name__ == '__main__':
 
     # 此处写死cnn的处理大小为7*7
     cnn_kernel = 7
-    all_models, model_test_x = get_trees(cnn_kernel, input_timesteps, train_all, test_all)
+    all_models = get_trees(cnn_kernel, input_timesteps, train_all, test_all)
 
-    # model reorganize to remove overlays (from 4 different 12*12 to 1 18*18)
+    # model reorganize to remove overlays (from 4 different 12*12 to 1 18*18), a function needed
 
-    predictions = predict_multiple(all_models, model_test_x, prediction_length)
+    predictions = predict_multiple(all_models, prediction_length, cnn_kernel, length, width)
