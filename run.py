c02fea933137e681ec90ca9a09c3b91677d4717a
Jingchao Yang <jcyangt@gmail.com>
2020-02-19 12:55:30 -0500

    save the prediction result

--- run.py
+++ run.py
@@ -27,6 +27,7 @@ from keras.layers import TimeDistributed
 from sklearn.metrics import r2_score
 import math
 from sklearn.metrics import mean_squared_error
+import pandas as pd
 
 
 def plot_results(predicted_data, true_data):
@@ -80,103 +81,120 @@ def main():
     configs = json.load(open('config.json', 'r'))
     if not os.path.exists(configs['model']['save_dir']): os.makedirs(configs['model']['save_dir'])
 
-    data = DataLoader(
-        os.path.join(configs['data']['filename']),
-        configs['data']['train_test_split'],
-        configs['data']['columns']
-    )
+    coors = ['9q5csxx', '9q5csz8', '9q5csz9', '9q5cszd', '9q5csze', '9q5cszs', '9q5cszt', '9q5csxr', '9q5csz2',
+             '9q5csz3', '9q5csz6', '9q5csz7', '9q5cszk', '9q5cszm', '9q5csxp', '9q5csz0', '9q5csz1', '9q5csz4',
+             '9q5csz5', '9q5cszh', '9q5cszj', '9q5cswz', '9q5csyb', '9q5csyc', '9q5csyf', '9q5csyg', '9q5csyu',
+             '9q5csyv', '9q5cswx', '9q5csy8', '9q5csy9', '9q5csyd', '9q5csye', '9q5csys', '9q5csyt', '9q5cswr',
+             '9q5csy2', '9q5csy3', '9q5csy6', '9q5csy7', '9q5csyk', '9q5csym', '9q5cswp', '9q5csy0', '9q5csy1',
+             '9q5csy4', '9q5csy5', '9q5csyh', '9q5csyj']
 
-    x_train, y_train, train_nor = data.get_data(
-        seq_len=configs['data']['sequence_length'],
-        normalise=configs['data']['normalise'],
-        tbd='train'
-    )
+    for coor in coors:
+        data = DataLoader(
+            os.path.join(configs['data']['filename']),
+            configs['data']['train_test_split'],
+            [coor]
+        )
 
-    x_test, y_test, test_nor = data.get_data(
-        seq_len=configs['data']['sequence_length'],
-        normalise=configs['data']['normalise'],
-        tbd='test'
-    )
+        x_train, y_train, train_nor = data.get_data(
+            seq_len=configs['data']['sequence_length'],
+            normalise=configs['data']['normalise'],
+            tbd='train'
+        )
 
-    model = Sequential()
-    # model.add(Masking(mask_value=-1, input_shape=(
-    #     configs['model']['layers'][0]['input_timesteps'], configs['model']['layers'][0]['input_dim'])))
-    model.add(LSTM(configs['model']['layers'][0]['neurons'], input_shape=(
-        configs['model']['layers'][0]['input_timesteps'], configs['model']['layers'][0]['input_dim']),
-                   return_sequences=configs['model']['layers'][0]['return_seq']))
-    model.add(Dropout(configs['model']['layers'][1]['rate']))
-    model.add(
-        LSTM(configs['model']['layers'][2]['neurons'], return_sequences=configs['model']['layers'][2]['return_seq']))
-    model.add(
-        LSTM(configs['model']['layers'][3]['neurons'], return_sequences=configs['model']['layers'][3]['return_seq']))
-    model.add(Dropout(configs['model']['layers'][4]['rate']))
-    model.add(Dense(configs['model']['layers'][5]['neurons'], activation=configs['model']['layers'][5]['activation']))
-    # plot_model(model.model, to_file='model_plot_test.png', show_shapes=True, show_layer_names=True)
+        x_test, y_test, test_nor = data.get_data(
+            seq_len=configs['data']['sequence_length'],
+            normalise=configs['data']['normalise'],
+            tbd='test'
+        )
 
-    model.compile(loss=configs['model']['loss'], optimizer=configs['model']['optimizer'])
+        model = Sequential()
+        # model.add(Masking(mask_value=-1, input_shape=(
+        #     configs['model']['layers'][0]['input_timesteps'], configs['model']['layers'][0]['input_dim'])))
+        model.add(LSTM(configs['model']['layers'][0]['neurons'], input_shape=(
+            configs['model']['layers'][0]['input_timesteps'], configs['model']['layers'][0]['input_dim']),
+                       return_sequences=configs['model']['layers'][0]['return_seq']))
+        model.add(Dropout(configs['model']['layers'][1]['rate']))
+        model.add(
+            LSTM(configs['model']['layers'][2]['neurons'],
+                 return_sequences=configs['model']['layers'][2]['return_seq']))
+        model.add(
+            LSTM(configs['model']['layers'][3]['neurons'],
+                 return_sequences=configs['model']['layers'][3]['return_seq']))
+        model.add(Dropout(configs['model']['layers'][4]['rate']))
+        model.add(
+            Dense(configs['model']['layers'][5]['neurons'], activation=configs['model']['layers'][5]['activation']))
+        # plot_model(model.model, to_file='model_plot_test.png', show_shapes=True, show_layer_names=True)
 
-    save_fname = os.path.join(configs['model']['save_dir'], '%s-e%s.h5' % (
-        dt.datetime.now().strftime('%d%m%Y-%H%M%S'), str(configs['training']['epochs'])))
+        model.compile(loss=configs['model']['loss'], optimizer=configs['model']['optimizer'])
 
-    callbacks = [
-        EarlyStopping(monitor='val_loss', patience=2),
-        ModelCheckpoint(filepath=save_fname, monitor='val_loss', save_best_only=True)
-    ]
+        save_fname = os.path.join(configs['model']['save_dir'], '%s-e%s.h5' % (
+            dt.datetime.now().strftime('%d%m%Y-%H%M%S'), str(configs['training']['epochs'])))
 
-    model.fit(
-        x_train,
-        y_train,
-        epochs=configs['training']['epochs'],
-        batch_size=configs['training']['batch_size'],
-        callbacks=callbacks
-    )
-    model.save(save_fname)
+        callbacks = [
+            EarlyStopping(monitor='val_loss', patience=2),
+            ModelCheckpoint(filepath=save_fname, monitor='val_loss', save_best_only=True)
+        ]
 
-    '''next n time-stamp prediction'''
-    predictions = predict_sequences_multiple(model, x_test, configs['data']['sequence_length'],
-                                             configs['data']['prediction_length'])
-    '''next single time-stamp prediction'''
-    # predictions = predict_point_by_point(model, x_test)
+        model.fit(
+            x_train,
+            y_train,
+            epochs=configs['training']['epochs'],
+            batch_size=configs['training']['batch_size'],
+            callbacks=callbacks
+        )
+        model.save(save_fname)
 
-    for yt in range(len(y_test)):
-        nor = test_nor[yt]
-        y_test[yt][0] = nor * (y_test[yt][0] + 1)
+        '''next n time-stamp prediction'''
+        predictions = predict_sequences_multiple(model, x_test, configs['data']['sequence_length'],
+                                                 configs['data']['prediction_length'])
+        '''next single time-stamp prediction'''
+        # predictions = predict_point_by_point(model, x_test)
 
-    for prdt in range(len(predictions)):
-        nor_ = test_nor[prdt * configs['data']['prediction_length']]
-        predictions[prdt] = [nor_ * (j + 1) for j in predictions[prdt]]
+        for yt in range(len(y_test)):
+            nor = test_nor[yt]
+            y_test[yt][0] = nor * (y_test[yt][0] + 1)
 
-    '''result plot'''
-    plot_results_multiple(predictions, y_test, configs['data']['prediction_length'])
+        for prdt in range(len(predictions)):
+            nor_ = test_nor[prdt * configs['data']['prediction_length']]
+            predictions[prdt] = [nor_ * (j + 1) for j in predictions[prdt]]
 
-    # model = Model()
-    # model.build_lstm(configs)
-    # plot_model(model.model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
-    #
-    # # in-memory training
-    # model.train(
-    #     x_train,
-    #     y_train,
-    #     epochs=configs['training']['epochs'],
-    #     batch_size=configs['training']['batch_size'],
-    #     save_dir=configs['model']['save_dir']
-    # )
-    #
-    # predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'],
-    #                                                configs['data']['sequence_length'])
-    #
-    # plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
+        '''result plot'''
+        # plot_results_multiple(predictions, y_test, configs['data']['prediction_length'])÷
 
-    '''result evaluation'''
-    # pred_multiple_all_merge = [j for i in predictions for j in i]
-    #
-    # testScore = math.sqrt(mean_squared_error(y_test[:len(pred_multiple_all_merge)], pred_multiple_all_merge))
-    # print('Test Score: %.2f RMSE' % (testScore))
-    #
-    # lstm_score = r2_score(y_test[:len(pred_multiple_all_merge)], pred_multiple_all_merge)
-    # print("R^2 Score of model = ", lstm_score)
+        '''save to csv'''
+        result = pd.DataFrame(index=np.arange(max(len(y_test), len(predictions))))
+        result['tru'] = pd.Series(y_test.reshape(y_test.size))
+        result['prediction'] = pd.Series([j for i in predictions for j in i])
+        result.to_csv('../IoT_HeatIsland_Data/data/LA/exp_data/result_single_point_prediction_LSTM/' + coor + '.csv')
 
-    # plot_results(predictions, y_test)
+        # model = Model()
+        # model.build_lstm(configs)
+        # plot_model(model.model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+        #
+        # # in-memory training
+        # model.train(
+        #     x_train,
+        #     y_train,
+        #     epochs=configs['training']['epochs'],
+        #     batch_size=configs['training']['batch_size'],
+        #     save_dir=configs['model']['save_dir']
+        # )
+        #
+        # predictions = model.predict_sequences_multiple(x_test, configs['data']['sequence_length'],
+        #                                                configs['data']['sequence_length'])
+        #
+        # plot_results_multiple(predictions, y_test, configs['data']['sequence_length'])
+
+        '''result evaluation'''
+        # pred_multiple_all_merge = [j for i in predictions for j in i]
+        #
+        # testScore = math.sqrt(mean_squared_error(y_test[:len(pred_multiple_all_merge)], pred_multiple_all_merge))
+        # print('Test Score: %.2f RMSE' % (testScore))
+        #
+        # lstm_score = r2_score(y_test[:len(pred_multiple_all_merge)], pred_multiple_all_merge)
+        # print("R^2 Score of model = ", lstm_score)
+
+        # plot_results(predictions, y_test)
 
 
 if __name__ == '__main__':
