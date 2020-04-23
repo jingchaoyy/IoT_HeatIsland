ae8675c17e0861ab8718aad5ed25ea32973c2073
jingchao yang <jcyangt@gmail.com>
2020-04-23 11:29:22 -0400

    adding offset UTC-7 for LA local

--- data_fusion/ground_truth.py
+++ data_fusion/ground_truth.py
@@ -5,14 +5,17 @@ Created on  1/14/20
 Ground truth data correlation and alignment test
 """
 import pandas as pd
+from datetime import datetime, timedelta
 
 iot_path = '/Volumes/Samsung_T5/IoT_HeatIsland_Data/data/LA/exp_data/tempMatrix_LA.csv'
-wu_path = '/Volumes/Samsung_T5/IoT_HeatIsland_Data/data/LA/weather_underground/LA/processed/KCALOSAN764.txt.csv'
+wu_path = '/Volumes/Samsung_T5/IoT_HeatIsland_Data/data/LA/weather_underground/LA/processed_updated/KCALOSAN764.txt.csv'
 
 iot_data = pd.read_csv(iot_path, usecols=['time', '9q5csyd'])
 iot_data['time'] = pd.to_datetime(iot_data['time'], format='%Y%m%d%H')
 wu_data = pd.read_csv(wu_path, usecols=['time', 'temperature'])
+wu_data['time'] = pd.DatetimeIndex(wu_data['time']) - timedelta(hours=7)
+wu_data['time'] = pd.to_datetime(wu_data['time'], format='%Y%m%d%H')
 
-joined = wu_data.set_index('time').join(iot_data.set_index('time'))
+joined = iot_data.set_index('time').join(wu_data.set_index('time'))
 
 print()
