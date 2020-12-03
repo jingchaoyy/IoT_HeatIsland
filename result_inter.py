0266b76a972335f0def072ce6bfc84df6b365f4f
Jingchao Yang <jcyangt@gmail.com>
2020-12-03 15:17:31 -0500

    updated

--- result_inter.py
+++ result_inter.py
@@ -8,35 +8,107 @@ import matplotlib.pyplot as plt
 import glob
 from tqdm import tqdm
 import numpy as np
+from sklearn.metrics import r2_score
+import os.path
+from os import path
 
-# result = pd.read_csv('/Volumes/Samsung_T5/IoT_HeatIsland_Data/data/zmodel_evaluation'
-#                      '/testerror_boxplot_Chicago_Chicago_allattr.csv')
-#
-# # sns.boxplot(x='RMSE', y='Temperature (F)', data=result)
-# result.boxplot()
-# plt.xlabel('input-->forecast')
-# plt.ylabel('Temperature (C)')
-# plt.title('Prediction RMSE (Chicago)')
-# plt.show()
 
-loc = 'LA_LA_20neuron'
-model_load_path = f'/Volumes/Samsung_T5/IoT_HeatIsland_Data/data/LA/exp_data/{loc}/'
+def all_RMSE(result_path, exp_list):
+    df = pd.DataFrame()
+    for exp in tqdm(range(len(exp_list))):
+        train_window, output_size = exp_list[exp]
+
+        rmse_path = glob.glob(result_path + f'\{str(train_window)}_{str(output_size)}' + r'\testScores_C.csv')
+        try:
+            df[f'{str(train_window)}_{str(output_size)}'] = pd.read_csv(rmse_path[0],
+                                                                             usecols=['0']).values.flatten()
+        except:
+            df[f'{str(train_window)}_{str(output_size)}'] = np.NaN
+
+    return df
+
+
+def all_r2(result_path, exp_list):
+    df = pd.DataFrame()
+    for exp in tqdm(range(len(exp_list))):
+        train_window, output_size = exp_list[exp]
+
+        pred_path = glob.glob(result_path + f'\{str(train_window)}_{str(output_size)}' + r'\pred.csv')
+        try:
+            pred_result = pd.read_csv(pred_path[0])
+            df[f'{str(train_window)}_{str(output_size)}'] = [r2_score(pred_result.pred, pred_result.ori)]
+        except:
+            df[f'{str(train_window)}_{str(output_size)}'] = np.NaN
+
+    return df
+
+
+def box_plot(df):
+    df.boxplot()
+    plt.xlabel('input-->forecast', size=20)
+    plt.ylabel('Temperature (C) RMSE', size=20)
+    plt.xticks(size=20)
+    plt.yticks(size=20)
+    plt.xticks(rotation=18)
+    plt.title(f'Local Testing RMSE', size=30)
+    # plt.legend(fontsize=40, loc=4)  # lower right
+    plt.show()
+
+
+def line_plot(fpath):
+    # df = pd.DataFrame()
+    df = pd.read_csv(fpath, index_col=['exp'])
+    # df[f'{name}_min'] = rmse_df.min().tolist()
+    # df[f'{name}_max'] = rmse_df.max().tolist()
+    # df.to_csv(r'E:\IoT_HeatIsland_Data\data\zmodel_evaluation\local_RMSE\all_minmax.csv', index=False)
+
+    # color_dict = {'LA_min': 'blue', 'NYC_min': 'orange', 'Atlanta_min': 'green', 'Chicago_min': 'red',
+    #               'LA_max': 'blue', 'NYC_max': 'orange', 'Atlanta_max': 'green', 'Chicago_max': 'red'}
+    color_dict = {'LA': 'blue', 'NYC': 'orange', 'Atlanta': 'green', 'Chicago': 'red'}
+
+    # use get to specify dark gray as the default color.
+    df.plot(color=[color_dict.get(x, '#333333') for x in df.columns])
+    plt.xlabel('input-->forecast', size=20)
+    plt.ylabel('R-square', size=20)
+    plt.xticks(size=20)
+    plt.yticks(size=20)
+    # plt.xticks(rotation=18)
+    plt.title(f'R-square by City', size=30)
+    plt.show()
+
+
+def save_RMSE(name, df, fpath):
+    if path.exists(fpath):
+        df_tem = pd.read_csv(fpath, index_col=['exp'])
+        df_tem[f'{name}_avg'] = df.mean()
+        df_tem.to_csv(fpath)
+    else:
+        df_tem = pd.DataFrame()
+        # df[f'{name}_min'] = rmse_df.min().tolist()
+        # df[f'{name}_max'] = rmse_df.max().tolist()
+        df_tem[f'{name}_avg'] = df.mean()
+        df_tem.index.name = 'exp'
+        df_tem.to_csv(fpath)
+
+
+city = 'Atlanta'
+model_load_path = f'E:\IoT_HeatIsland_Data\data\{city}\exp_data\{city}_{city}_12neuron'
 
 experiments = [(24, 1), (24, 4), (24, 8), (24, 12), (36, 12), (48, 12), (72, 12), (72, 24), (72, 36), (72, 48),
-               (120, 48), (144, 48), (120, 72), (144, 72), (168, 120)]
+               (120, 48), (144, 48), (120, 72), (144, 72), (168, 120), (240, 120)]
 
-rmse_df = pd.DataFrame()
-for exp in tqdm(range(len(experiments))):
-    train_window, output_size = experiments[exp]
+'''RMSE box plot'''
+# rmse_df = all_RMSE(model_load_path, experiments)
+# box_plot(rmse_df)
 
-    rmse_path = glob.glob(model_load_path + f'{str(train_window)}_{str(output_size)}/' + 'testScores_C.csv')
-    try:
-        rmse_df[f'{str(train_window)}_{str(output_size)}'] = pd.read_csv(rmse_path[0], usecols=['0']).values.flatten()
-    except:
-        rmse_df[f'{str(train_window)}_{str(output_size)}'] = np.NaN
+'''Save RMSE'''
+rmse_df = all_RMSE(model_load_path, experiments)
+save_RMSE(city, rmse_df, r'E:\IoT_HeatIsland_Data\data\zmodel_evaluation\local_RMSE\all_avg.csv')
 
-rmse_df.boxplot()
-plt.xlabel('input-->forecast')
-plt.ylabel('Temperature (C)')
-plt.title(f'Prediction RMSE ({loc})')
-plt.show()
+'''RMSE city comparison'''
+# rmse_df = all_RMSE(model_load_path, experiments)
+# line_plot(city, rmse_df)
+
+'''R2 city comparison'''
+# r2_df = all_r2(model_load_path, experiments)
+# line_plot(r'E:\IoT_HeatIsland_Data\data\zmodel_evaluation\local_RMSE\All_r2.csv')
