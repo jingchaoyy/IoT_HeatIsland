77881115afb374920cc445dea24759077111f9ce
Jingchao Yang <jcyangt@gmail.com>
2020-12-10 15:33:40 -0500

    customized plot

--- result_inter.py
+++ result_inter.py
@@ -43,14 +43,14 @@ def all_r2(result_path, exp_list):
     return df
 
 
-def box_plot(df):
+def box_plot(df, name):
     df.boxplot()
     plt.xlabel('input-->forecast', size=20)
     plt.ylabel('Temperature (C) RMSE', size=20)
-    plt.xticks(size=20)
+    plt.xticks(size=15)
     plt.yticks(size=20)
-    plt.xticks(rotation=18)
-    plt.title(f'Local Testing RMSE', size=30)
+    # plt.xticks(rotation=18)
+    plt.title(f'Local Testing RMSE ({name})', size=30)
     # plt.legend(fontsize=40, loc=4)  # lower right
     plt.show()
 
@@ -91,24 +91,78 @@ def save_RMSE(name, df, fpath):
         df_tem.to_csv(fpath)
 
 
-city = 'Atlanta'
+city = 'Chicago'
 model_load_path = f'E:\IoT_HeatIsland_Data\data\{city}\exp_data\{city}_{city}_12neuron'
 
 experiments = [(24, 1), (24, 4), (24, 8), (24, 12), (36, 12), (48, 12), (72, 12), (72, 24), (72, 36), (72, 48),
                (120, 48), (144, 48), (120, 72), (144, 72), (168, 120), (240, 120)]
 
 '''RMSE box plot'''
-# rmse_df = all_RMSE(model_load_path, experiments)
-# box_plot(rmse_df)
+rmse_df = all_RMSE(model_load_path, experiments)
+box_plot(rmse_df, city)
 
 '''Save RMSE'''
-rmse_df = all_RMSE(model_load_path, experiments)
-save_RMSE(city, rmse_df, r'E:\IoT_HeatIsland_Data\data\zmodel_evaluation\local_RMSE\all_med.csv')
+# rmse_df = all_RMSE(model_load_path, experiments)
+# save_RMSE(city, rmse_df, r'E:\IoT_HeatIsland_Data\data\zmodel_evaluation\local_RMSE\all_avg.csv')
 
 '''RMSE city comparison'''
 # rmse_df = all_RMSE(model_load_path, experiments)
 # line_plot(city, rmse_df)
 
+rmse_avg = pd.read_csv(r'E:\IoT_HeatIsland_Data\data\zmodel_evaluation\local_RMSE\all_avg.csv')
+rmse_max = pd.read_csv(r'E:\IoT_HeatIsland_Data\data\zmodel_evaluation\local_RMSE\all_max.csv')
+rmse_min = pd.read_csv(r'E:\IoT_HeatIsland_Data\data\zmodel_evaluation\local_RMSE\all_min.csv')
+
+cities = ['LA', 'NYC', 'Atlanta', 'Chicago']
+color = ['blue', 'orange', 'green', 'red']
+
+fig, ax = plt.subplots()
+for city in range(len(cities)):
+    ax.plot(rmse_avg['exp'],
+            rmse_avg[cities[city]],
+            '-',
+            label=cities[city],
+            linewidth=5.0)
+    ax.fill_between(rmse_avg['exp'],
+                    rmse_min[cities[city]],
+                    rmse_max[cities[city]],
+                    alpha=0.2,
+                    facecolor=color[city])
+plt.legend(fontsize=30)
+plt.xlabel('input-->forecast', size=30)
+plt.ylabel('Temperature (C) RMSE', size=30)
+plt.xticks(size=30)
+for label in ax.get_xaxis().get_ticklabels()[::2]:
+    label.set_visible(False)
+plt.yticks(size=30)
+plt.title(f'RMSE by City', size=30)
+
+plt.show()
+
 '''R2 city comparison'''
 # r2_df = all_r2(model_load_path, experiments)
 # line_plot(r'E:\IoT_HeatIsland_Data\data\zmodel_evaluation\local_RMSE\All_r2.csv')
+
+r2 = pd.read_csv(r'E:\IoT_HeatIsland_Data\data\zmodel_evaluation\local_RMSE\All_r2.csv')
+
+cities = ['LA', 'NYC', 'Atlanta', 'Chicago']
+color = ['blue', 'orange', 'green', 'red']
+
+fig, ax = plt.subplots()
+for city in range(len(cities)):
+    ax.plot(r2['exp'],
+            r2[cities[city]],
+            '-',
+            label=cities[city],
+            linewidth=5.0)
+
+plt.legend(fontsize=30)
+plt.xlabel('input-->forecast', size=30)
+plt.ylabel('R-square', size=30)
+plt.xticks(size=30)
+for label in ax.get_xaxis().get_ticklabels()[::2]:
+    label.set_visible(False)
+plt.yticks(size=30)
+plt.title(f'R-square by City', size=30)
+
+plt.show()
